96fe891773cd7a3aa69e1a623ab4d2cbf17aadc7
NikitaChernobryvka <chernobryvka.mykyta@student.uzhnu.edu.ua>
2026-03-15 21:37:00 +0200

    Додано пакет model з класами Category та Purchase

---
+++ src/main/java/com/purchasehistorysystem/model/Category.java
@@ -0,0 +1,42 @@
+package com.purchasehistorysystem.model;
+
+public class Category {
+    private int id;
+    private String name;
+    private String iconPath;
+
+    public Category() {
+
+    }
+
+    public Category(int id, String name, String iconPath) {
+        this.id = id;
+        this.name = name;
+        this.iconPath = iconPath;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getIconPath() {
+        return iconPath;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setIconPath(String iconPath) {
+        this.iconPath = iconPath;
+    }
+}
+
