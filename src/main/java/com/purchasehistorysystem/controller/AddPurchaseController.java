f1bf6315aa3c80ee55c010af59e64f5a28926e21
NikitaChernobryvka <chernobryvka.mykyta@student.uzhnu.edu.ua>
2026-03-27 20:53:45 +0200

    Створено контроллер для додавання нових категорій

--- src/main/java/com/purchasehistorysystem/controller/AddPurchaseController.java
+++ src/main/java/com/purchasehistorysystem/controller/AddPurchaseController.java
@@ -5,10 +5,15 @@ import com.purchasehistorysystem.model.Purchase;
 import com.purchasehistorysystem.repository.CategoryRepository;
 import com.purchasehistorysystem.repository.PurchaseRepository;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.List;
@@ -24,6 +29,8 @@ public class AddPurchaseController {
 
     private void loadCategories() {
         try {
+            categoryComboBox.getItems().clear();
+
             List<Category> categoryList = categoryRepository.getAllCategories();
 
             categoryComboBox.getItems().addAll(categoryList);
@@ -67,6 +74,31 @@ public class AddPurchaseController {
 
         catch (SQLException exception) {
             System.out.println("Помилка при збереженні нової покупки");
+        }
+    }
+
+    @FXML public void onAddCategoryButton() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
+                    "/com/purchasehistorysystem/templates/AddCategoryDialog.fxml")
+            );
+
+            VBox dialogContent = fxmlLoader.load();
+
+            Stage dialogStage = new Stage();
+            Scene dialogScene = new Scene(dialogContent);
+
+            dialogStage.setTitle("Створити категорію");
+            dialogStage.initModality(Modality.APPLICATION_MODAL);
+            dialogStage.setScene(dialogScene);
+            dialogStage.setWidth(500);
+            dialogStage.showAndWait();
+
+            loadCategories();
+        }
+
+        catch (IOException exception) {
+            System.out.println("Помилка під час викриття діалогового вікна");
         }
     }
 }
